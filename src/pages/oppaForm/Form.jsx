8e04b0ec7581285369134114ad4cf4089436c364
Baiaz Orozbekov <baiaz.orozbekov@gmai.com>
2024-05-22 18:53:56 +0600

    can send data

--- src/pages/oppaForm/Form.jsx
+++ src/pages/oppaForm/Form.jsx
@@ -3,8 +3,28 @@ import { motion } from "framer-motion";
 import './form.css'
 import oppaBanner from '../../img/oppaBanner.png'
 import cloud from '../../img/cloud.svg'
+import { useState } from "react";
+import axios from "axios";
 
 function Form() {
+
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const data = {
+            Name: name,
+            Phone: phone
+        }
+
+        axios.post('https://sheet.best/api/sheets/93a33348-f545-4509-ace9-9060dfa283fc', data).then((res) => {
+            console.log(res);
+            setName('')
+            setPhone('')
+        })
+    }
+
     return (
         <div className='oppa__possibility_3 section__padding' id='possibility'>
                 <div className="oppa__possibility_3-content">
@@ -25,12 +45,12 @@ function Form() {
                     <div className='oppa__possibility_3-content_2'>
                         <h1 className='gradient__text'>Oppa для бизнеса</h1>
                         <p>Станьте нашим партнером</p>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <p className="form_p">Как вас зовут</p>
-                            <input type="text" placeholder="Ваше Имя"/>
+                            <input type="text" placeholder="Ваше Имя" onChange={e => setName(e.target.value)} value={name}/>
                             <p className="form_p">Ваш номер телефона</p>
-                            <input type="text" placeholder="Ваш номер телефона" />
-                            <input className="form_submit" type="submit" placeholder="Отправить" />
+                            <input type="text" placeholder="Ваш номер телефона" onChange={e => setPhone(e.target.value)} value={phone}/>
+                            <input className="form_submit" type="submit" placeholder="Отправить"/>
                         </form>
                     </div>
                 </div>
